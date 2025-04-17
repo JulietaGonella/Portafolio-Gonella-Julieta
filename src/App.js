fce20d3217d1f137b966511b693aac2ce187cc4b
JulietaGonella <julietagonella2001@hotmail.com>
2025-04-17 14:11:20 -0300

    Actualización del portafolio con iconos y mejoras de estilo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 import julieta from './assets/julieta.jpg'; // guardá tu foto en src/assets
+import htmlIcon from './assets/icons/html-5_5968267.ico';
+import cssIcon from './assets/icons/css_919826.ico';
+import jsIcon from './assets/icons/javascript_15525512.ico';
+import nodeIcon from './assets/icons/nodejs.ico';
+import mysqlIcon from './assets/icons/database_16550682.ico';
+
 
 function App() {
     return (
@@ -11,21 +17,9 @@ function App() {
                     <div className="texto">
                         <h1>¡Hola! Mi nombre es Julieta Gonella.</h1>
                         <p className="intro">
-                            Soy estudiante en analista en sistema y entusiasta del desarrollo web.
-                            Me encanta aprender cosas nuevas y enfrentar desafíos que me ayuden a crecer profesionalmente.
-                        </p>
-                        <h2>Descripcion personal</h2>
-                        <p className="descripcion">
-                            Soy una persona curiosa, responsable y con muchas ganas de seguir creciendo en el mundo del desarrollo web.
-                            Me encuentro estudiando para ser Analista en Sistemas, y mientras tanto voy adquiriendo experiencia
-                            a través de proyectos y prácticas que me ayudan a aplicar lo aprendido.
-                            <br /><br />
-                            Me interesa especialmente el diseño de interfaces intuitivas, la programación del lado del cliente y el trabajo en equipo.
-                            Hasta la actualidad aprendí tecnologías como HTML, CSS, JavaScript y me entusiasma seguir avanzando
-                            con frameworks y herramientas modernas.
-                            <br /><br />
-                            Busco oportunidades para poner en práctica mis conocimientos, seguir aprendiendo y aportar valor
-                            desde mi compromiso y creatividad.
+                        Soy estudiante de Analista en Sistemas, entusiasta del desarrollo web y con experiencia en HTML, 
+                        CSS y JavaScript. Me gusta aprender, asumir desafíos y trabajar en equipo, siempre buscando crecer 
+                        y aportar con creatividad.
                         </p>
                     </div>
                     <div className="foto">
@@ -58,13 +52,14 @@ function App() {
                         repartidores, con un enfoque en la organización y la simplicidad.
                     </p>
                     <h4>Tecnologías usadas:</h4>
-                    <ul>
-                        <li>HTML</li>
-                        <li>JavaScript</li>
-                        <li>CSS</li>
-                        <li>Node JS</li>
-                        <li>MySQL</li>
-                    </ul>
+                    <div className="iconos-tecnologias">
+                        <img src={htmlIcon} alt="HTML" title="HTML" />
+                        <img src={jsIcon} alt="JavaScript" title="JavaScript" />
+                        <img src={cssIcon} alt="CSS" title="CSS" />
+                        <img src={nodeIcon} alt="Node.js" title="Node.js" />
+                        <img src={mysqlIcon} alt="MySQL" title="MySQL" />
+                    </div>
+
                     <div className="botones">
                         <a
                             href="https://drive.google.com/file/d/1qzxWkgmTBpMAlPRdE-_PQPtuT9K1qtlT/view?usp=drive_link"
